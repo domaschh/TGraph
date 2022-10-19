cb2d73492d8e30fa2e91d9b1cb6e633575a2cfb7
thomasmages <t.mages@firestart.com>
2022-10-19 23:17:01 +0200

    feat: initial grpah implementation

--- src/main.rs
+++ src/main.rs
@@ -27,48 +27,54 @@ struct Node {
     id: usize,
     edges: RefCell<Vec<Rc<Edge>>>
 }
+
+impl Node {
+    fn addEdge(&self, edge: Rc<Edge>) {
+    self.edges.borrow_mut().push(edge)
+    }
+    
+    fn new(id: usize) -> Rc<Self> {
+        Rc:: new(Node {
+            id, 
+            edges: RefCell::new(vec![])
+        })
+    }
+    
+    fn remove_edge(&self, edge: Rc<Edge>) {
+        self.edges.borrow_mut().iter().filter(|listEdge|!Rc::ptr_eq(listEdge, &edge));
+    }
+}
+
 impl Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,"Node: {}", self.id);
+        write!(f,"Node: {}, Connections:[", self.id);
         for edge in self.edges.borrow().iter(){
             write!(f, "{}", edge);
         }
+        println!("]");
         Ok(())
     }
 }
 
-impl Node {
-    fn new(id: usize) -> Self {
-        Node {
-            id, 
-            edges: RefCell::new(vec![])
-        }
-    }
-
-    fn addEdge(&self, edge: Rc<Edge>) {
-        self.edges.borrow_mut().push(edge)
-    }
-
-    fn replaceEgde(&self, edge: Edge) {
-
-    }
-}
-
 fn main() {
-    let node1 = Rc::new(Node::new(1));
-    let node2 = Rc::new(Node::new(2));
+    let node1 =Node::new(1);
+    let node2 = Node::new(2);
     
     let edge1 = Edge::new(1, node2.clone());
     let edge2 = Edge::new(2, node1.clone());
     
-    node2.addEdge(edge2.clone());
+   /*  node2.addEdge(edge2.clone());
     node1.addEdge(edge1.clone());
     
     println!("{}", node1);
     println!("{}", node1);
-    edge1.change_weight(5);
+    edge1.change_weight(5); */
 
+
+    node1.remove_edge(edge1.clone());
+    print!("-----------------------\n");
     println!("{}", node1);
-    println!("{}", node2);
-
+    node1.addEdge(edge1.clone());
+    print!("-----------------------\n");
+    println!("{}", node1);
 }
